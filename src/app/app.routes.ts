219050d70bb6371b0cd8c74fd80095c821505bf3
hanadigaribovic <hanadi.garibovic@stu.ssst.edu.ba>
2025-05-28 17:06:59 +0200

    Bakend and frontend integration for Auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/auth/login-page/login-page.component';
+import { RegisterPageComponent } from './components/auth/register-page/register-page.component';
 import { PlatformLayoutComponent } from './components/platform-layout/platform-layout.component';
 import { BooksPageComponent } from './components/books-page/books-page.component';
 import { BookComponent } from './components/books-page/book/book.component';
--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,22 +1,32 @@
-import {ChangeDetectorRef, Component} from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
   form: FormGroup;
   showPassword = false;
   wrongPassword = false;
 
-
-  constructor(private fb: FormBuilder, private router: Router, private cdr: ChangeDetectorRef) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private cdr: ChangeDetectorRef,
+    private authService: AuthService
+  ) {
     this.form = this.fb.group({
       login: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
@@ -37,19 +47,15 @@ export class LoginPageComponent {
     if (this.form.valid) {
       const { login, password } = this.form.value;
 
-      const isAuthenticated = login === 'john@doe.com' && password === 'password123';
-
-      if (isAuthenticated) {
-        this.router.navigate(['/home']);
-      } else {
-        this.wrongPassword = true;
-
-        setTimeout(() => {
+      this.authService.login({ email: login, password }).subscribe({
+        next: () => this.router.navigate(['/books']), // ili gde god ideš posle logina
+        error: () => {
+          this.wrongPassword = true;
           this.form.get('password')?.setValue('');
           this.form.get('password')?.markAsTouched();
-          this.wrongPassword = false;
-        }, 2500);
-      }
+          this.cdr.detectChanges(); // refreshuje template da prikaže poruku
+        },
+      });
     }
   }
 
