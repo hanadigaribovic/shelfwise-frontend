e33b27541014fba3237bfb7886cdf7934ec769c5
hanadigaribovic <118663134+hanadigaribovic@users.noreply.github.com>
2025-06-02 12:28:07 +0200

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 (function (window) {
   window["env"] = window["env"] || {};
   window["env"]["NG_APP_API_URL"] =
-    "https://shelfwise-backend-05e96e925358.herokuapp.com";
+    "https://shelfwise-backend-05e96e925358.herokuapp.com/api";
 })(this);
