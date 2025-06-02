8a55f101d6596105847b4e4b95674bc8d91b14dd
hanadigaribovic <hanadi.garibovic@stu.ssst.edu.ba>
2025-06-02 10:45:21 +0200

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://shelfwise-backend-05e96e925358.herokuapp.com',
+  // apiUrl: 'https://shelfwise-backend-05e96e925358.herokuapp.com',
+  apiUrl:
+    (window as any)['env']['NG_APP_API_URL'] ||
+    'https://shelfwise-backend-05e96e925358.herokuapp.com',
 };
