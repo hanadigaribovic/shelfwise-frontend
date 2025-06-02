5b00b6b13ad0c54a5d7f6152f26270fb8b0f7ad7
hanadigaribovic <118663134+hanadigaribovic@users.noreply.github.com>
2025-06-02 12:27:49 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   // apiUrl: 'https://shelfwise-backend-05e96e925358.herokuapp.com',
   apiUrl:
     (window as any)['env']['NG_APP_API_URL'] ||
-    'https://shelfwise-backend-05e96e925358.herokuapp.com',
+    'https://shelfwise-backend-05e96e925358.herokuapp.com/api',
 };
